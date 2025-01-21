605a24d85f21f5b17c783e18c51d5735cb7d0ce3
28choucroute28 <julien.alleaume.282@gmail.com>
2025-01-21 16:31:12 +0100

    Correction CI ajout package.json et eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
-const html = require("eslint-plugin-html");
+const htmlPlugin = require("eslint-plugin-html");
 
-module.exports = [
-  {
-    files: ["*.html"],
-    plugins: {
-      html,
+module.exports = {
+  overrides: [
+    {
+      files: ["*.html"],
+      parser: "eslint-plugin-html", // Utilise eslint-plugin-html pour parser le HTML
+      plugins: ["html"], // Assure-toi que le plugin html est ajouté
+      rules: {
+        // Tu peux ajouter des règles spécifiques ici
+      },
     },
-    languageOptions: {
-      parser: html,
-    },
-  },
-];
+  ],
+};
+
